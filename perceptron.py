7f9cf955b43129ebdc958dc13aaf4a2a48544735
avery <avrypromo@pm.me>
2021-06-15 17:06:17 -0700

    iterate deprecated

--- perceptron.py
+++ perceptron.py
@@ -20,10 +20,10 @@ from plot import plot
 ##########################################################
 # Program Layout
 # 1. perceptron calls format to make sure the data is compatible with the algorithm
-# 2. perceptron calls iterate to evaluate a single data point
-# 3. iterate calls evaluate, which determines if the classifier guessed the data's label correctly
-# 4. perceptron updates the classifier with theta += y_i*x_i and theta_0 += y_i.
-# iterate returns zeroes if the guess was accurate
+# 2. perceptron calls evaluate
+# 3. evaluate determines if the classifier guessed the data's label correctly
+# 4. evaluate returns relevant data to update classifier parameters with
+# 5. perceptron updates the classifier with theta += y_i*x_i and theta_0 += y_i.
 ##########################################################
 
 
@@ -59,7 +59,7 @@ def format(data, theta, theta_0):
     return (data, theta, theta_0)
 
 def evaluate(data_i, theta, theta_0):
-    """determines whether or not the algorithm guessed properly
+    """determines whether or not the classifier was correct, and returns relevant data
 
     Args:
         data_i (numpy array): data hyperpoint
@@ -67,31 +67,19 @@ def evaluate(data_i, theta, theta_0):
         theta_0 (float): 0d classifier parameter
 
     Returns:
-        int: -1 if guessed incorrectly, 1 otherwise
+        tuple or False: interpreted by perceptron
     """
-    # data_i[1] is y_i, the algorithm's guess.
-    return data_i[1]*np.sign(np.dot(theta, data_i[0]) + theta_0)
+    # data_i[0] is x_i, the hyperpoint
+    # data_i[1] is y_i, the label for that hyperpoint (-1 or 1)
+    guess = data_i[1]*np.sign(np.dot(theta, data_i[0]) + theta_0)
 
-
-def iterate(data_i, theta, theta_0):
-    """pipes perceptron output to the classifier after evaluation
-
-    Args:
-        data_i (numpy array): data hyperpoint
-        theta (numpy array): classifier hyperparameters
-        theta_0 (float): 0d classifier parameter
-
-    Returns:
-        tuple: result of a perceptron iteration
-    """
-    if evaluate(data_i, theta, theta_0) <= 0:
-        # adjustment necessary (return actual values)
+    if guess <= 0:
         return (data_i, theta, theta_0)
     else:
-        # no adjustment
         return False
 
-def perceptron(data, theta=None, theta_0=0, t=1000):
+
+def perceptron(data, theta=None, theta_0=0, t=1):
     """introductory supervised machine learning algorithm to generate a linear classifier in n-dimensional space
 
     Args:
@@ -103,30 +91,37 @@ def perceptron(data, theta=None, theta_0=0, t=1000):
     Returns:
         tuple: a tuple containing the resulting classifier (hyper)parameters
     """
-    theta, theta_0 = np.zeros_like(data[0][0]), 0
+
+    if theta == None:
+        theta, theta_0 = np.zeros_like(data[0][0]), 0
 
     data, theta, theta_0 = format(data, theta, theta_0)
 
     index = 0
+
     # looping this makes classifier more accurate
-    # for iteration in range(t):
-    for point in data:
-        index += 1
+    for iteration in range(t):
+        for point in data:
 
-        # print(f"new data point: {point}")
-        # print(f"{theta} is theta, {theta_0} is theta_0")
-        result = iterate(point, theta, theta_0)
+            index += 1
+            print(f"Loop number {index}")
+            print(f"{theta} is theta, {theta_0} is theta_0")
+            print(f"new data point. y_i: {point[1]} x_i {point[0]}")
 
-        if result != False:
-            # theta += y_i * x_i; the algorithm updating itself
-            # numpy doesn't like casting floats to ints, hence not "theta += result[0][1]*result[0][0]""
-            np.add(theta, result[0][1]*result[0][0], out=theta, casting='unsafe')
-            theta_0 += result[2]
-            # print(f"{theta} is new theta, {theta_0} is new theta_0")
 
-        # matplotlib.pyplot.plot unfortunately takes a 'label' kwarg to name each entry in a legend
-        # do not confuse it with the labels attached to supervised learning data
-        plot(theta, theta_0, label=str(index))
+            result = evaluate(point, theta, theta_0)
+
+            if result != False:
+                # theta += y_i * x_i; the algorithm updating itself
+                # yixi is y_i * x_i
+                yixi = np.multiply(result[0][1], result[0][0])
+                # numpy doesn't like casting floats to ints, hence not "theta += result[0][1]*result[0][0]""
+                np.add(theta, yixi, out=theta, casting='unsafe')
+                theta_0 += result[0][1]
+
+            # matplotlib.pyplot.plot unfortunately takes a 'label' kwarg to name each entry in a legend
+            # do not confuse it with the labels attached to supervised learning data
+            plot(theta, theta_0, label=str(index))
 
 
     return (theta, theta_0)
