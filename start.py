51bf580dcbb2fba35326f44562ff206dbd989e4e
avery <avrypromo@pm.me>
2021-07-06 14:56:22 -0700

    title, formatting, and dataset

--- start.py
+++ start.py
@@ -5,13 +5,13 @@ import perceptron
 
 
 data = [
+    [np.array([[1], [-1]]), 1] ,
     [np.array([[0], [1]]), -1] ,
-    [np.array([[-1.5], [-1]]), 1] ,
-    [np.array([[1], [-1]]), 1]
+    [np.array([[-10], [-1]]), 1]
 ]
 
-print("Results! Theta = {}, theta_0 = {}".format(*perceptron.perceptron(data)))
+print("Classifier: Theta.T = {.T}, theta_0 = {}".format(*perceptron.perceptron(data)))
 
 plt.legend()
-plt.title("Perceptron")
+plt.title("Perceptron Classifiers")
 plt.show()
