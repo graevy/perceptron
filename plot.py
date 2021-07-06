51471847adfaa31f6ea2451cfdf6961983747b8e
avery <avrypromo@pm.me>
2021-07-06 15:04:17 -0700

    ndplot maybe

--- plot.py
+++ plot.py
@@ -28,56 +28,56 @@ def plot(theta, theta_0=0, label=''):
     plt.plot([xmin, xmax], [y1, y2], label=label)
 
 ##########################################
-# A better but untested implementation
+# A better but untested implementation?
 ##########################################
-def bounds(data, extra=True):
-    xmin, xmax = 0, 0
+# def bounds(data, extra=True):
+#     xmin, xmax = 0, 0
 
-    for item in data:
-        if item[0][0] < xmin:
-            xmin = item[0][0]
-        elif item[0][0] > xmax:
-            xmax = item[0][0]
+#     for item in data:
+#         if item[0][0] < xmin:
+#             xmin = item[0][0]
+#         elif item[0][0] > xmax:
+#             xmax = item[0][0]
 
-    if extra:
-        return (xmin-1, xmax+1)
-    else:
-        return (xmin, xmax)
-
-
-# TODO
-def ndplot(theta, theta_0=0, label='', data=None):
-    if data != None:
-        xmin, xmax = bounds(data)
-        dimensions = len(data[0][0])
-    else:
-        xmin, xmax = -5, 5
-        dimensions = 2
+#     if extra:
+#         return (xmin-1, xmax+1)
+#     else:
+#         return (xmin, xmax)
 
 
-    if dimensions == 2:
-        xvalues = np.linspace(xmin, xmax)
-        yvalues = []
+# # TODO
+# def ndplot(theta, theta_0=0, label='', data=None):
+#     if data != None:
+#         xmin, xmax = bounds(data)
+#         dimensions = len(data[0][0])
+#     else:
+#         xmin, xmax = -5, 5
+#         dimensions = 2
 
-        # for each x value in xvalues, θ.T • X = -θ_0
-        for x in xvalues:
-            # solve for the y coordinate where (θ.T)•X = -θ_0
-            # θ = [a,b], coords = [x,y]
-            # so ax + by = -θ_0,
-            # y = -(θ_0 + ax) / b
-            # plot it with pyplot:
-            yvalues.append(  -(theta_0 + theta[0][0]*x) / theta[1][0]  )
+
+#     if dimensions == 2:
+#         xvalues = np.linspace(xmin, xmax)
+#         yvalues = []
+
+#         # for each x value in xvalues, θ.T • X = -θ_0
+#         for x in xvalues:
+#             # solve for the y coordinate where (θ.T)•X = -θ_0
+#             # θ = [a,b], coords = [x,y]
+#             # so ax + by = -θ_0,
+#             # y = -(θ_0 + ax) / b
+#             # plot it with pyplot:
+#             yvalues.append(  -(theta_0 + theta[0][0]*x) / theta[1][0]  )
         
-        plt.plot(xvalues, yvalues, label=label)
+#         plt.plot(xvalues, yvalues, label=label)
 
-    else:
-        pass # TODO
-        # 3d seems much trickier, but really you just have to call 2d plot on a series of lines to get a plane
-        # this lends itself well to nd recursion (e.g. yielding 3d spaces for a 4d theta)
-        # dot product of 2d vectors [a,b] and [c,d] is ac + bd
-        # dot product of 3d vectors [a,b,c] and [d,e,f] is ad + be + cf
-        # dot product of nd vectors va: [a, ..., n] and vb: [b, ..., n] is:
-        # sum([va[index] * vb[index] for index in range(n)])
-        # Theta.T • X + Theta_0 = 0
-        # Theta.T • X = -Theta_0
-        # n = len(theta)
+#     else:
+#         pass # TODO
+#         # 3d seems much trickier, but really you just have to call 2d plot on a series of lines to get a plane
+#         # this lends itself well to nd recursion (e.g. yielding 3d spaces for a 4d theta)
+#         # dot product of 2d vectors [a,b] and [c,d] is ac + bd
+#         # dot product of 3d vectors [a,b,c] and [d,e,f] is ad + be + cf
+#         # dot product of nd vectors va: [a, ..., n] and vb: [b, ..., n] is:
+#         # sum([va[index] * vb[index] for index in range(n)])
+#         # Theta.T • X + Theta_0 = 0
+#         # Theta.T • X = -Theta_0
+#         # n = len(theta)
